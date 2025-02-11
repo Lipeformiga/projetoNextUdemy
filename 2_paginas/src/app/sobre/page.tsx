2655a365f550a27191aa5d95db2efe7ee152530b
Filipe Formigari <formigari2804@gmail.com>
2025-02-10 21:13:02 -0300

    a

---
+++ 2_paginas/src/app/sobre/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1>Página de sobre</h1>
+        <Link href="/">Home</Link>
+    </div>
+  )
+}
+
+export default page
